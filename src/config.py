e0eaab9afd884628a71905568b56548ca0772b9b
Daniel Palomino <dapalominop@gmail.com>
2021-01-07 22:25:45 -0500

    [ADD] Added class to manage datasets.

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import transformers
 MAX_LEN = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SICE = 8
-EPOCHS = 8
+EPOCHS = 10
 BASE_MODEL_PATH = "../models/bert_base_uncased"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "../datasets/ner_dataset.csv"
